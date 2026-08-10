b69b4970298d3cb1271519666223495678d9d2cd
Ivan Snopov <TheCoolLogin@gmail.com>
2026-08-11 02:55:11 +0300

    ContentItem stable id

--- app/src/main/java/com/whats/notesapp/domain/model/ContentItem.kt
+++ app/src/main/java/com/whats/notesapp/domain/model/ContentItem.kt
@@ -1,8 +1,18 @@
 package com.whats.notesapp.domain.model
 
+import java.util.UUID
+
 sealed interface ContentItem {
 
-    data class Text(val text: String) : ContentItem
+    val id: String
 
-    data class Image(val url: String) : ContentItem
+    data class Text(
+        val text: String,
+        override val id: String = UUID.randomUUID().toString()
+    ) : ContentItem
+
+    data class Image(
+        val url: String,
+        override val id: String = UUID.randomUUID().toString()
+    ) : ContentItem
 }
